8db5245178af1663c9a4f115ee143234fa657182
JakkarinK <earth1277@gmail.com>
2023-01-04 16:04:17 +0700

    forth commit

--- src/contents/Experiences/Experiences.js
+++ src/contents/Experiences/Experiences.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import CardExp from './CardExp'
 import './experiences.css'
+import DreamPic from './img/Dreamlendar.png'
+import RoomraiPic from './img/Roomrai.png'
+import PortPic from './img/Port.jpg'
+import WebsitePic from './img/website.png'
+import TeaPic from './img/Tea.png'
 
 function Experiences() {
     const expList = [
         //name, description, cover, use_lang
         [
+            'Blog Website',
+            'Blog website is a website me training to do, I do fullstack-developer.',
+            WebsitePic,
+            ['HTML', 'CSS', 'JavaScript', 'ReactJS']
+            
+        ],
+        [
             'Portfolio Website',
             'Portfolio Website is my portfolio, I created for keep your memory.',
-            'https://via.placeholder.com/250x200',
+            PortPic,
             ['HTML', 'CSS', 'JavaScript', 'ReactJS']
         ],
         [
             'Roomrai',
             'Roomrai is a navigate application for people can\'t find my room in universal, I do part backend-developer.',
-            'https://via.placeholder.com/250x200',
-            ['Dart', 'SQLite', 'Google Map API']
+            RoomraiPic,
+            ['Dart', 'Flutter', 'SQLite', 'Google Map API']
         ],
         [
             'Dreamlendar',
             'Dreamlendar is a calendar application, it have so many template to used you can customize by yourself, I do part backend-developer.',
-            'https://via.placeholder.com/250x200',
-            ['Dart', 'SQLite']
+            DreamPic,
+            ['Dart', 'Flutter', 'SQLite']
         ],
         [
-            'Coffee Market',
-            'Coffee Market is a store website online, you can buy coffee in my website, I do fullstack-developer.',
-            'https://via.placeholder.com/250x200',
+            'TEA Cafe',
+            'TEA Cafe is a store website online, you can buy coffee in my website, I do fullstack-developer.',
+            TeaPic,
             ['HTML', 'CSS', 'JavaScript']
             
         ],
@@ -35,9 +47,10 @@ function Experiences() {
         <section className='experiences'>
             <div className="container">
                 <CardExp expList={expList[0]}/> {/* Blog Website */}
-                <CardExp expList={expList[1]}/> {/* Roomrai */}
-                <CardExp expList={expList[2]}/> {/* Dreamlendar */}
-                <CardExp expList={expList[3]}/> {/* Coffee Market */}
+                <CardExp expList={expList[1]}/> {/* Portfolio */}
+                <CardExp expList={expList[2]}/> {/* Roomrai  */}
+                <CardExp expList={expList[3]}/> {/* Dreamlendar*/}
+                <CardExp expList={expList[4]}/> {/* Coffee Market */}
             </div>
         </section>
     )
--- src/contents/Profile/Profile.js
+++ src/contents/Profile/Profile.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import ProfilePic from './profile.jpg';
 import './profile.css';
+import Resume from './Resume_JakkarinK.pdf'
 
 function Profile() {
     return (
         <section className='profile'>
             <div className='container'>
-                <div className="topic">
-                    <h1>Hi' my name is</h1>
-                    <h2>Jakkarin Kerdthaidee</h2>
-                    <p>I'm 3rd-year Software Engineering student,
-                    at Thammasat University.<br/>
-                    This is my portfolio, Thank you for visited. 
-                    </p>
-                </div>
+                <div className="detail">
+                    <div className="topic">
+                        <h1>Hi' my name is</h1>
+                        <h2>Jakkarin Kerdthaidee</h2>
+                        <p>I'm 3rd-year Software Engineering student,
+                            at Thammasat University.<br />
+                            This is my portfolio, Thank you for visited.
+                        </p>
+                    </div>
                     <img src={ProfilePic} alt="profile" />
+                </div>
+                <a href={Resume}>Resume</a>
             </div>
         </section>
     )
